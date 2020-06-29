1f60b71f7fb3e820d98a0f6acac8fdc74bc8851c
wiryfuture <wiryfuture@gmail.com>
2020-06-29 23:34:54 +0100

    Set up for help from normal people

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 // Load up the discord.js library
-const Discord = require("discord.js");
+const discord = require("discord.js");
 const global = require("global")
 const document = require("global/document")
 const window = require("global/window")
 const fs = require('fs');
+const enmap = require("enmap");
+
+// Load dotenv
+// Access by process.env.{KEY}
+const config = require('dotenv').config();
+// process.env.token {string} bot token
+// process.env.prefix {string} default bot prefix
+// process.env.bannedwordslistname {string} default blacklisted words file
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
+const client = new discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-// old boomer code :: const config = require("./config.json");
-const { prefix, token, bannedwordslistname } = require("./config.json");
+// Wait for enmap to be loaded 
+/*
+myenmap.defer.then( () => {
+    console.log(myenmap.size + " keys loaded");
+}); 
+*/
+// Set enmap settings
+client.settings = new enmap({
+    name: "settings",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep'
+});
 
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
-// config.bannedwordslistname contains the name of the file with the list of banned words
+client.wordfilter = new enmap({ 
+    name: "wordfilter",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep'
+});
+
+// Default config for servers
+const defaultsettings = {
+    prefix: "!",
+    modlogchannel: "mod-log",
+    modrole: "Moderator",
+    adminrole: "Admin",
+    welcomechannel: "welcome",
+    welcomemessage: "Say hello to {{user}}, everyone!",
+    filterwords: false,
+    dowelcomemessage: false,
+    usecustomfilterlist: false
+};
+
+const defaultwordfilter = {
+    filename: "bannedwords.txt",
+    words: ""
+};
+
 
 //tries to get the list of banned words from local file
-function getbannedwords(){
-    fs.readFile(bannedwordslistname, "utf8", (err, data) => {
+function getbannedwords(filename){
+    fs.readFile(filename, "utf8", (err, data) => {
         console.log("Read word file successfully!");
-        global.bannedwords = data.split("\r\n");
-        console.log("Successfully got " + global.bannedwords.length.toString() + " banned words from the list.");
+        bannedwords = data.split("\r\n");
+        console.log("Successfully got " + bannedwords.length + " banned words from the list.");
+        return bannedwords;
     });
 
 }
@@ -37,10 +78,13 @@ function mystatus(client){
         }
 }
 
+
+
+
 client.on("ready", () => {
     // This event will run if the bot starts, and logs in, successfully.
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    getbannedwords();
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    var defaultbannedwords = getbannedwords("bannedwords.txt");
     // Example of changing the bot's playing game to something useful. `client.user` is what the
     // docs refer to as the "ClientUser".
     mystatus(client);
@@ -57,8 +101,8 @@ client.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
     mystatus(client);
+    client.settings.delete(guild.id);
 });
-
 
 client.on("message", async message => {
     // This event will run on every single message received, from any channel or DM.
@@ -72,44 +116,88 @@ client.on("message", async message => {
     // Also good practice to ignore any message that does not start with our prefix, 
     // which is set in the configuration file.
     //if (message.content.indexOf(config.prefix) !== 0) return;
+    const guild = message.guild || null;
+
+    if (guild == null) {
+        return message.reply("Seems you've slid into my DMs, unfortunately, I can't actually read anything you send here (yet!), sowwy  :sob: ");
+    }
+    
+    // Get the server settings, or get the default settings
+    const guildconf = client.settings.ensure(message.guild.id, defaultsettings);
+
+    const guildwordfilter = client.wordfilter.ensure(message.guild.id, defaultwordfilter);
 
     // Here we separate our "command" name, and our "arguments" for the command. 
     // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
     // args = ["Is", "this", "the", "real", "life?"]
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(guildconf.prefix.length).trim().split(/ +/g);
+    // command = say   
     const command = args.shift().toLowerCase();
-    const guild = message.guild || null;
-
     const messageasargs = message.content.trim().split(/ +/g);
     // dontsay : variable to let bot send message
     global.dontsay = 200;
-    // checks if you have the role to bypass the filter
-    if (!message.member.roles.cache.some(r => ["Moderator"].includes(r.name))) {
-        // For each banned word, 
-        global.bannedwords.forEach(element => {
-            // For each word in the message, check if they are equal.
-            messageasargs.forEach(word => {
-                lowercaseword = word.toLowerCase();
-                if (lowercaseword == element) {
-                    message.author.send("Chill buddy, saying \"" + element + "\" isn't allowed!");
+    // checks if you have the role to bypass the filter (by default them mod role)
+    //if (!message.member.roles.cache.some(r => ["Moderator"].includes(r.name))) {
+    // For each banned word, 
+
+    if (guildconf.filterwords) {
+        // Checks messages against the default word filter
+        if (!guildconf.usecustomfilterlist) {
+            defaultbannedwords.forEach(element => {
+                // assumes that the message does not violate the blacklist by default
+                violation = false;
+                // For each word in the message, check if they are equal to said banned word.
+                messageasargs.forEach(word => {
+                    lowercaseword = word.toLowerCase();
+                    if (lowercaseword == element) {
+                        violation = true;
+                    }
+                });
+                // searches a message for a banned word or phrase, returns -1 if it does not
+                // gets the whole message in lower case for further analysis
+                lowercasemessage = message.content.toLowerCase();
+                // searches for the banned word or phrase
+                containsbannedphrase = lowercasemessage.search(element);
+                // length of the banned word
+                lenghtofbannedword = element.length;
+
+                if (containsbannedphrase != -1) {
+                    violation = true;
+                }
+                // Acts if the message contains a banned word
+                if (violation) {
                     message.delete().catch(O_o => { });
                     global.dontsay = 403;
+                    message.author.send("Chill buddy, saying \"" + element + "\" isn't allowed!\nIf you think that this word or phrase should not be blacklisted, first please consider:\nIs it offensive?\nIs it distressing?\nIs it a sensitive topic that shouldn't be discussed here?");
                 }
             });
-            /*
-            lowercasemessagecontent = message.content.toLowerCase();
-            if (lowercasemessagecontent.includes(element)) {
-                message.author.send("Chill buddy, saying \"" + element + "\" isn't allowed!");
-                message.delete().catch(O_o => { });
-                global.dontsay = 403;
+        }
+        // Else, check messages against custom word filter, which will be empty by default
+        else {
+            // Checks if a filename is provided for presets
+            if (guildwordfilter.filename) {
+                // Try use the word file with this name
+                try {
+                    bannedword = getbannedwords()
+                }
+                // Use default word file
+                catch {
+
+                }
             }
-            */
-        });
+            // Use word list in guildwordfilter.words
+            else {
+
+            }
+        }
+        
     }
+    
+    
+    //}
 
     // After this, the bot doesn't care about messages without a prefix
-    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.content.indexOf(guildconf.prefix) !== 0) return;
 
     // Let's go with a few common example commands! Feel free to delete or change those.
 
@@ -148,11 +236,11 @@ client.on("message", async message => {
 
     // lets a moderator reload the list of banned words (example use: the list has been updates)
     if (command === "reloadbannedwords") {
-        if (!message.member.roles.some(r => ["Moderator"].includes(r.name)))
+        if (!message.member.roles.cache.some(r => ["Moderator"].includes(r.name)))
             return message.reply("Sorry, you don't have permissions to use this!");
         // rus the function to get the list of banned words
-        getbannedwords();
-        message.channel.send("Reloaded banned words list.");
+        bannedwords = getbannedwords();
+        message.channel.send("Reloaded words from list.");
     }
 
     if (command === "kick") {
@@ -255,6 +343,7 @@ client.on("message", async message => {
 
     }
 
+    // Changes the colour of the role picked
     if (command == "changerolecolour") {
         if (!args[0]) {
             return message.reply("Please give me a colour or do RANDOM, more info here https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable.");
@@ -267,21 +356,78 @@ client.on("message", async message => {
 
         let role = guild.roles.cache.find(role => role.name === "quirky");
 
-        role.setColor({ newrolecolour })
-            .then(message.channel.send("Changed role colour to " + newrolecolour))
-            .catch(message.reply("Please make sure you're providing a valid colour, more info on colours here https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable."));
+        role.setColor(newrolecolour)
+            .then(message.channel.send("Changed quirky colour to " + newrolecolour))
+            .catch(function (e) {
+                if (e instanceof TypeError) {
+                    message.reply("Please make sure you're providing a valid colour, more info on colours here https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable.");
+                }
+                else {
+                    message.reply("An unknown error occurred, try again later, then tell us.");
+                }
+            });
 
     }
+    
+    // Adds a word to the banned word list and reloads it, updating it on the fly
+    if (command == "addbannedword") {
+        if (!args[0]) {
+            return message.reply("You at least need to give me a word, to be able to do this.");
+        }
+    }
 
+    // Command to set config
+    if (command == "setconf") {
+        if (!message.member.roles.cache.some(r => [guildconf.adminrole].includes(r.name))) {
+            return message.reply("You're not an admin, sowwy :(");
+        }
+        // Splits message into arguments
+        const [prop, ...value] = args;
+        // If the setting/key is not found
+        if(!client.settings.has(message.guild.id, prop)) {
+            return message.reply("This key is not in the configuration.");
+        }
+        // Applies setting change
+        client.settings.set(message.guild.id, value.join(" "), prop);
+        // Confirms everything worked and the value changed
+        message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+    }
+
+    // Add a setting after an update, hopefully can be deprecated because   u   g   l   y
+    if (command == "addconf") {
+        if (!message.member.roles.cache.some(r => [guildconf.adminrole].includes(r.name))) {
+            return message.reply("You're not an admin, sowwy :(");
+        }
+         // Splits message into arguments
+         const [prop, ...value] = args;
+         // Applies setting change
+         client.settings.set(message.guild.id, value.join(" "), prop);
+         // Confirms everything worked and the value changed
+         message.channel.send(`Guild configuration item ${prop} has been changed to:\n\`${value.join(" ")}\``);
+    }
+
+    // Shows a server's config
+    if(command === "showconf") {
+        if (!message.member.roles.cache.some(r => [guildconf.adminrole] == r.name)) {
+            return message.reply("You're not an admin, sorry :(");
+        }
+        let configProps = Object.keys(guildconf).map(prop => {
+          return `${prop}  :  ${guildconf[prop]}\n`;
+        });
+        message.channel.send(`The following are the server's current configuration:\`\`\`${configProps}\`\`\``);
+      }
     
 
     // Debugging command to check things
 
     if (command == "debug") {
-        message.reply(args[0]);
+        let configProps = Object.keys(guildconf).map(prop => {
+            return `${prop}  :  ${guildconf[prop]}\n`;
+          });
+        message.channel.send(`The following are the server's current configuration:\`\`\`${configProps}\`\`\``);
     }
     
     // End of code that manipulates messages
 });
 
-client.login(token);
+client.login(process.env.token);
