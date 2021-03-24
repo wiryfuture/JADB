84083d76f6536b92e4e8c41b331fc2318ba22650
wiryfuture <wiryfuture@gmail.com>
2021-03-24 18:40:42 +0000

    Removed unecessary file

--- source/types/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { Client, Message } from "discord.js";
-
-declare module commands {
-    export async function execute(client: Client, message: Message, args: Array<String>): Promise<any>;
-}
